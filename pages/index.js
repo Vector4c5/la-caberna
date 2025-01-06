ab63b8ff80c43b9a7c317fe209b572eaadc8a917
Vector4c5 <hernandez435627@gmail.com>
2025-01-05 20:58:35 -0600

    add: styles

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,48 @@
 import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import logo from "../public/logo_la_caberna.jpeg";
+import cabaña from "../public/la_caberna2.jpeg";
 
 export default function Home() {
+  
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <div className="flex flex-col min-h-screen w-full bg-white p-0">
+        <div id="Encabezado" className="relative flex w-full h-96 items-center justify-between  bg-green-300">
+          <Image src={cabaña} alt="Cabaña" className="absolute inset-0 w-full h-full object-cover" />         
+          <div id="text-of-Encabezado" className="relative">
+            <div className="object-left-bottom flex flex-col">
+              <h1 className="text-3xl font-bold text-white">La Caberna</h1>
+              <p className="text-white">¡¡Únete a la aventura!!</p>
+            </div>
+            <Image src={logo} alt="Logo" className="w-40 h-40 object-right-bottom" />
+          </div>
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+        <div id="Introduccion" className="h-1/2 flex flex-col items-center justify-center bg-blue-500 p-4">
+          <h1 className="text-2xl font-bold">Aquí irá una breve descripción acerca de la página web y de lo que se quiere lograr</h1>
+          <p>Los juegos de rol son divertidos</p>
+        </div>
+
+        <div id="Enlaces" className="h-1/2 flex flex-row items-center justify-center bg-red-500 p-4">
+          <a id="Jugadores" className="mb-4">
+            <h1 className="text-xl font-bold">Aprende a jugar</h1>
+            <p>Echa un vistazo a nuestras guías</p>
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+          <a id="Masters" className="mb-4">
+            <h1 className="text-xl font-bold">Crea tu próxima aventura</h1>
+            <p>Ven y nutre tu imaginación con lo que tenemos para ti</p>
+          </a>
+
+          <a id="Comunidad">
+            <h1 className="text-xl font-bold">Únete a nuestra comunidad</h1>
+            <p>Únete a nosotros y vive grandes aventuras</p>
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
+
+
+     
+
