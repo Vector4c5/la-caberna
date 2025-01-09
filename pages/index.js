f078a1924d95431cc0f39aa55e44b2e2b92ffd4e
Vector4c5 <hernandez435627@gmail.com>
2025-01-09 00:14:06 -0600

    add: StartAnimation.js and fix: MessageButton.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import Fondo_bienvenida from "../public/img_inicio.jpeg";
 import Fondo_interior_caverna from "../public/img_interior_caberna.jpeg";
 import NeonButton from "@/components/common/NeonButton";
-import SeccionButton from "@/components/common/SeccionButton"; // Importar el nuevo botón
-import { Toaster } from 'react-hot-toast'; // Importar Toaster
-import MessageButton from "@/components/common/MessageButton ";
+import SeccionButton from "@/components/common/SeccionButton";
+import MessageButton from "@/components/common/MessageButton";
+import StarAnimation from "@/components/common/StartAnimation"; // Importar el componente
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   const [showFirst, setShowFirst] = useState(true);
@@ -15,11 +16,16 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center relative w-full h-screen bg-black">
+      {/* Animación de Estrellas */}
+      
+
+      {/* Primera Sección */}
       <div
         className={`absolute transition-opacity duration-700 ${
           showFirst ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
         } min-w-full min-h-screen flex flex-col items-center justify-center`}
       >
+        <StarAnimation />
         <Image
           src={Fondo_bienvenida}
           alt="Descripción de la imagen"
@@ -29,8 +35,8 @@ export default function Home() {
           ref={containerRef}
           className="absolute flex flex-col items-center justify-center border-4 border-white rounded-full"
           style={{
-            width: 'clamp(200px, 30vw, 500px)',
-            aspectRatio: '1',
+            width: "clamp(200px, 30vw, 500px)",
+            aspectRatio: "1",
           }}
         >
           <h1 className="text-center font-bold text-white text-7xl">La Caverna</h1>
@@ -46,6 +52,7 @@ export default function Home() {
         </div>
       </div>
 
+      {/* Segunda Sección */}
       <div
         className={`absolute transition-opacity duration-700 ${
           !showFirst ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
@@ -57,29 +64,22 @@ export default function Home() {
           className="relative opacity-25 h-screen z-2"
         />
         <div className="flex flex-col absolute object-center">
-        <div>
-            {/* Usando NeonButton */}
-            <NeonButton href="https://www.ejemplo.com">
-              Visitar Ejemplo
-            </NeonButton>
-          </div>
-
           <div>
-            {/* Usando MessageButton con mensaje personalizado */}
-            <MessageButton message="¡Bienvenido a la Caverna, aventura te espera!" >
+            <NeonButton href="https://www.ejemplo.com">Visitar Ejemplo</NeonButton>
+          </div>
+          <div>
+            <MessageButton message="¡Bienvenido a la Caverna, aventura te espera!">
               ¡Haz clic aquí para empezar!
             </MessageButton>
           </div>
           <div>
-            {/* Usando SeccionButton con mensaje personalizado y enlace */}
-            <SeccionButton 
-              message="¡Bienvenido a la caverna! Estás a punto de embarcarte en una gran aventura." 
+            <SeccionButton
+              message="¡Bienvenido a la caverna! Estás a punto de embarcarte en una gran aventura."
               link="https://www.ejemplo.com"
             >
               ¡Haz clic para empezar tu aventura!
             </SeccionButton>
           </div>
-
           <h1 className="text-4xl text-white">Segunda Sección</h1>
           <p className="text-lg text-white">Esta es la segunda sección visible.</p>
         </div>
